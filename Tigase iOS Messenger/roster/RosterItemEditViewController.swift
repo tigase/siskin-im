f00c4f79b06f16c360cce2e4f459255aaf66c4c7
Andrzej Wójcik <andrzejw@tigase.org>
2016-05-13 17:56:43 +0200

    Task #4135 - fixed licensing header in RosterItemEditViewController

--- Tigase iOS Messenger/roster/RosterItemEditViewController.swift
+++ Tigase iOS Messenger/roster/RosterItemEditViewController.swift
@@ -1,9 +1,22 @@
 //
-//  RosterItemEditViewController.swift
-//  Tigase-iOS-Messenger
+// RosterItemEditViewController.swift
 //
-//  Created by Andrzej Wójcik on 12.05.2016.
-//  Copyright © 2016 Tigase, Inc. All rights reserved.
+// Tigase iOS Messenger
+// Copyright (C) 2016 "Tigase, Inc." <office@tigase.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. Look for COPYING file in the top folder.
+// If not, see http://www.gnu.org/licenses/.
 //
 
 import UIKit
