ea178a067836a527822e2fb4e169da8b5d028a64
Andrzej Wójcik <andrzejw@tigase.org>
2016-05-18 19:41:25 +0200

    Task #4178 - fixed compilation issue

--- Tigase iOS Messenger/database/DBRoomsManager.swift
+++ Tigase iOS Messenger/database/DBRoomsManager.swift
@@ -1,15 +1,28 @@
 //
-//  DBRoomsManager.swift
-//  Tigase-iOS-Messenger
+// DBRoomsManager.swift
 //
-//  Created by Andrzej Wójcik on 16.05.2016.
-//  Copyright © 2016 Tigase, Inc. All rights reserved.
+// Tigase iOS Messenger
+// Copyright (C) 2016 "Tigase, Inc." <office@tigase.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. Look for COPYING file in the top folder.
+// If not, see http://www.gnu.org/licenses/.
 //
 
 import Foundation
 import TigaseSwift
 
-public class DBRoomsManager: AbstractRoomsManager {
+public class DBRoomsManager: DefaultRoomsManager {
     
     private let store: DBChatStore;
     
