6ac5e4ce2050f176080c2e4903aa407c1d54cfa7
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2019-02-01 00:10:33 +0100

    #8537: fixed DarkMode on the MUC room join dialog

--- Tigase iOS Messenger/chats/MucJoinViewController.swift
+++ Tigase iOS Messenger/chats/MucJoinViewController.swift
@@ -22,7 +22,7 @@
 import UIKit
 import TigaseSwift
 
-class MucJoinViewController: UITableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class MucJoinViewController: CustomTableViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     var xmppService:XmppService!;
     
