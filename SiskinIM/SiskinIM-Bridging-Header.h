e785947e25fd43a0a94748c0be3f4947ab1f32e9
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2019-04-24 13:46:07 +0200

    #8901, #tigaseiosmessenger-123: updated license header in source code files to GPLv3

--- SiskinIM/SiskinIM-Bridging-Header.h
+++ SiskinIM/SiskinIM-Bridging-Header.h
@@ -1,27 +1,27 @@
 //
-// Tigase-iOS-Messenger-Bridging.h
+// SiskinIM-Bridging-Header.h
 //
-// Tigase iOS Messenger
+// Siskin IM
 // Copyright (C) 2016 "Tigase, Inc." <office@tigase.com>
 //
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License,
-// or (at your option) any later version.
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
+// GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Affero General Public License
+// You should have received a copy of the GNU General Public License
 // along with this program. Look for COPYING file in the top folder.
-// If not, see http://www.gnu.org/licenses/.
+// If not, see https://www.gnu.org/licenses/.
 //
 
 
-#ifndef Tigase_iOS_Messenger_Bridging_h
-#define Tigase_iOS_Messenger_Bridging_h
+#ifndef SiskinIM_Bridging_h
+#define SiskinIM_Bridging_h
 
 #import "sqlite3.h"
 
@@ -29,4 +29,4 @@ typedef void (*sqlite3_destructor_type)(void*);
 #define SQLITE_STATIC      ((sqlite3_destructor_type)0)
 #define SQLITE_TRANSIENT   ((sqlite3_destructor_type)-1)
 
-#endif /* Tigase_iOS_Messenger_Bridging_h */
+#endif /* SiskinIM_Bridging_h */
--- SiskinIM/ui/RoundButton.swift
+++ SiskinIM/ui/RoundButton.swift
@@ -1,9 +1,22 @@
 //
-//  RoundButton.swift
-//  Tigase-iOS-Messenger
+// RoundButton.swift
 //
-//  Created by Andrzej Wójcik on 06/01/2019.
-//  Copyright © 2019 Tigase, Inc. All rights reserved.
+// Siskin IM
+// Copyright (C) 2016 "Tigase, Inc." <office@tigase.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. Look for COPYING file in the top folder.
+// If not, see https://www.gnu.org/licenses/.
 //
 
 import UIKit
