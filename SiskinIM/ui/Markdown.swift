2cbb00d1b937bf1376427d2cd989f7342472a7b9
mimi89999 <michel@lebihan.pl>
2021-02-23 20:38:11 +0100

    Change Markdown to XEP-393 styling

--- SiskinIM/ui/Markdown.swift
+++ SiskinIM/ui/Markdown.swift
@@ -81,6 +81,7 @@ class Markdown {
     static let SPACE: unichar = " ";
     static let ASTERISK: unichar = "*";
     static let UNDERSCORE: unichar = "_";
+    static let TILDE: unichar = "~";
     static let GRAVE_ACCENT: unichar = "`";
     static let CR_SIGN: unichar = "\r";
     
@@ -91,7 +92,7 @@ class Markdown {
         
         var boldStart: Int? = nil;
         var italicStart: Int? = nil;
-        var underlineStart: Int? = nil;
+        var strikeStart: Int? = nil;
         var quoteStart: Int? = nil;
         var quoteLevel = 0;
         var idx = 0;
@@ -105,6 +106,9 @@ class Markdown {
         
         while idx < message.length {
             let c = message.character(at: idx);
+            if idx + 1 < message.length && message.character(at: idx + 1) == c {
+                canStart = false;
+            }
             switch c {
             case GT_SIGN:
                 if quoteStart == nil && (idx == 0 || message.character(at: idx-1) == NEW_LINE) {
@@ -121,115 +125,96 @@ class Markdown {
                     }
                 }
             case ASTERISK:
-                let nidx = idx + 1;
-                if nidx < message.length, message.character(at: nidx) == ASTERISK {
-                    if boldStart == nil {
-                        if canStart {
-                            boldStart = idx;
-                        }
-                    } else {
-                        msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: boldStart!, length: (nidx+1) - idx));
-                        msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: idx, length: (nidx+1) - idx));
-                        
-                        
-                        msg.enumerateAttribute(.font, in: NSRange(location: boldStart!, length: (nidx+1) - boldStart!), options: .init()) { (attr, range: NSRange, stop) -> Void in
-                            let font = attr as? UIFont;
-                            let boldFont = Markdown.bold(font: font ?? defFont);
-                            msg.addAttribute(.font, value: boldFont, range: range);
-                        }
-                        
-                        boldStart = nil;
-                    }
-                    canStart = true;
-                    idx = nidx;
-                } else {
-                    if italicStart == nil {
-                        if canStart {
-                            italicStart = idx;
-                        }
-                    } else {
-                        msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: italicStart!, length: 1));
-                        msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: idx, length: 1));
-                        
-                        msg.enumerateAttribute(.font, in: NSRange(location: italicStart!, length: (idx+1) - italicStart!), options: .init()) { (attr, range: NSRange, stop) -> Void in
-                            let font = attr as? UIFont;
-                            let italicFont = Markdown.italic(font: font ?? defFont);
-                            msg.addAttribute(.font, value: italicFont, range: range);
-                        }
-                        
-                        italicStart = nil;
-                    }
-                    canStart = true;
-                }
-            case UNDERSCORE:
-                if underlineStart == nil {
+                if boldStart == nil {
                     if canStart {
-                        underlineStart = idx;
+                        boldStart = idx + 1;
                     }
                 } else {
-                    msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: underlineStart!, length: 1));
+                    msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: boldStart! - 1, length: 1));
                     msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: idx, length: 1));
+
+                    msg.enumerateAttribute(.font, in: NSRange(location: boldStart!, length: idx - boldStart!), options: .init()) { (attr, range: NSRange, stop) -> Void in
+                        let font = attr as? UIFont;
+                        let boldFont = Markdown.bold(font: font ?? defFont);
+                        msg.addAttribute(.font, value: boldFont, range: range);
+                    }
                     
-                    msg.addAttribute(.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: underlineStart!, length: idx - underlineStart!));
-                    underlineStart = nil;
+                    boldStart = nil;
+                }
+                canStart = true;
+            case UNDERSCORE:
+                if italicStart == nil {
+                    if canStart {
+                        italicStart = idx + 1;
+                    }
+                } else {
+                    msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: italicStart! - 1, length: 1));
+                    msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: idx, length: 1));
+
+                    msg.enumerateAttribute(.font, in: NSRange(location: italicStart!, length: idx - italicStart!), options: .init()) { (attr, range: NSRange, stop) -> Void in
+                        let font = attr as? UIFont;
+                        let italicFont = Markdown.italic(font: font ?? defFont);
+                        msg.addAttribute(.font, value: italicFont, range: range);
+                    }
+
+                    italicStart = nil;
+                }
+                canStart = true;
+            case TILDE:
+                if strikeStart == nil {
+                    if canStart {
+                        strikeStart = idx + 1;
+                    }
+                } else {
+                    msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: strikeStart! - 1, length: 1));
+                    msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: idx, length: 1));
+
+                    msg.addAttribute(.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: strikeStart!, length: idx - strikeStart!));
+
+                    strikeStart = nil;
                 }
                 canStart = true;
             case GRAVE_ACCENT:
-//                if codeStart == nil {
-                    if canStart {
-                        let codeStart = idx;
-                        let isBlock = 0 == idx || (message.character(at: idx-1) == NEW_LINE) || (idx > 3 && message.length > (idx + 1) && message.character(at: idx + 1) == SPACE && message.character(at: idx-2) == GT_SIGN && (0 == idx - 3 || message.character(at: idx - 3) == NEW_LINE));
-                        wordIdx = nil;
-                        while idx < message.length, message.character(at: idx) == "`" {
-                            idx = idx + 1;
-                        }
-                        let codeCount = idx - codeStart;
-                        print("code tag count = ", codeCount);
-                        
-                        var count = 0;
-                        while idx < message.length {
-                            if message.character(at: idx) == GRAVE_ACCENT {
-                                count = count + 1;
-                                if count == codeCount {
-                                    let tmp = idx + 1;
-                                    if tmp == message.length || [" ", "\n"].contains(message.character(at: tmp)) {
-                                        break;
-                                    }
+                if canStart {
+                    let codeStart = idx;
+                    wordIdx = nil;
+                    while idx < message.length, message.character(at: idx) == "`" {
+                        idx = idx + 1;
+                    }
+                    let codeCount = idx - codeStart;
+                    print("code tag count = ", codeCount);
+                    
+                    var count = 0;
+                    while idx < message.length {
+                        if message.character(at: idx) == GRAVE_ACCENT {
+                            count = count + 1;
+                            if count == codeCount {
+                                let tmp = idx + 1;
+                                if tmp == message.length || [" ", "\n"].contains(message.character(at: tmp)) {
+                                    break;
                                 }
-                            } else {
-                                count = 0;
                             }
-                            idx = idx + 1;
-                        }
-                        if codeCount != count {
-                            idx = codeStart + codeCount;
                         } else {
-                            msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: codeStart, length: codeCount));
-                            msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: (idx+1)-codeCount, length: codeCount));
+                            count = 0;
+                        }
+                        idx = idx + 1;
+                    }
+                    if codeCount != count {
+                        idx = codeStart + codeCount;
+                    } else {
+                        msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: codeStart, length: codeCount));
+                        msg.addAttribute(.foregroundColor, value: stylingColor, range: NSRange(location: (idx+1)-codeCount, length: codeCount));
 
-                            let codeFont = Markdown.code(font: defFont);
-                            msg.addAttribute(.font, value: codeFont, range: NSRange(location: codeStart, length: idx - codeStart));
+                        let codeFont = Markdown.code(font: defFont);
+                        msg.addAttribute(.font, value: codeFont, range: NSRange(location: codeStart, length: idx - codeStart));
 
-                            if isBlock {
-                                msg.addAttribute(.paragraphStyle, value: codeParagraphStyle, range: NSRange(location: codeStart, length: idx - codeStart));
-                            }
-                                                                                    
-                            if idx - codeStart > 1 {
-                                let clearRange = NSRange(location: codeStart + codeCount, length: idx - (codeStart + (2*codeCount)));
-                                //msg.removeAttribute(.foregroundColor, range: clearRange);
-                                msg.removeAttribute(.underlineStyle, range: clearRange);
-                                //msg.addAttribute(.foregroundColor, value: textColor ?? NSColor.textColor, range: clearRange);
-                            }
-                            
-                            if idx == message.length {
-                                wordIdx = message.length;
-                            } else {
-                                wordIdx = idx + 1;
-                            }
+                        if idx == message.length {
+                            wordIdx = message.length;
+                        } else {
+                            wordIdx = idx + 1;
                         }
                     }
-//                } else {
-//                }
+                }
                 canStart = true;
             case CR_SIGN, NEW_LINE, SPACE:
                 if showEmoticons {
@@ -253,8 +238,8 @@ class Markdown {
                 }
                 if NEW_LINE == c {
                     boldStart = nil;
-                    underlineStart = nil;
-                    italicStart = nil
+                    italicStart = nil;
+                    strikeStart = nil;
                     if (quoteStart != nil) {
                         print("quote level:", quoteLevel);
                         if idx < message.length {
